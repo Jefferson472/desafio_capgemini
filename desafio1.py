4ede5a49be208c5059589f1782aeb3941dbee71b
Jefferson472 <jefferson472@hotmail.com>
2022-03-01 22:47:04 -0300

    fix desafio 1 para passar no teste

--- desafio1.py
+++ desafio1.py
@@ -2,16 +2,19 @@ class CriaEscada():
     def desenha_escada(self, n):
         '''Recebe um número inteiro (n) e retorna uma escada desenhada em *'''
         base_escada = n
+        escada = ''
         for n in range(n, 0, -1):
             for j in range(0, base_escada, 1):
                 if j+1 < n:
-                    print(' ', end='')
+                    escada += (' ')
                 else:
-                    print('*', end='')
-            print()
+                    escada += ('*')
+            escada += '\n'
+        return escada
 
 
 if __name__ == '__main__':
     n = int(input('Digite um número inteiro: '))
     e = CriaEscada()
-    e.desenha_escada(n)
+    escada = e.desenha_escada(n)
+    print(escada)
